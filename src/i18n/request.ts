07d115bbc47b779827bb9cddf02cf7dd9bf5b95f
Hugo Cruz <hugocruzlfc@gmail.com>
2024-10-14 09:48:44 +0200

    feat: add next intl routes

---
+++ src/i18n/request.ts
@@ -0,0 +1,15 @@
+import { getRequestConfig } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ locale }) => {
+  if (!routing.locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (
+      await (locale === "en"
+        ? import("../../messages/en.json")
+        : import(`../../messages/${locale}.json`))
+    ).default,
+  };
+});
